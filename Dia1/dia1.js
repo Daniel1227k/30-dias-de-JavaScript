bcfd1e95b09087228302c55dd9fa1f3e34c6dba5
Daniel1227k <dc049709@gmail.com>
2025-03-25 01:37:39 -0600

    Add Day 1

---
+++ Dia1/dia1.js
@@ -0,0 +1,47 @@
+//1. Escribe un comentario de una sola línea que diga "los comentarios pueden hacer que el código sea legible"
+
+//2. Escribe otro comentario que diga, "Bienvenido a 30DaysOfJavaScript"
+//Bienvenido a 30 dias de JavaScript
+
+//3. Escribe un comentario multilínea que diga, "Los comentarios pueden hacer el código legible, fácil de usar e informativo"
+
+//4. Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+
+let cadena = "Hola";
+let boleano = true;
+let indefinido = undefined;
+let nulo = null;
+
+//5 Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+
+console.log(typeof cadena);
+console.log(typeof boleano);
+console.log(typeof indefinido);
+console.log(typeof nulo);
+
+//6. Declara cuatro variables sin asignar valores
+let var1;
+let var2;
+let var3;
+let var4;
+
+//7. Declara cuatro variables con valores asignados
+let valor1 = "uno";
+let valor2 = "dos";
+let valor3 = "tres";
+let valor4 = "cuatro";
+
+//8. Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas
+
+let nombre = "Daniel";
+let apellido = "Cruz";
+let estadoCivil = "Soltero";
+let pais = "Mexico";
+let edad = 22;
+
+//9. Declara dos variables miEdad y tuEdad y asígneles valores iniciales y regístrese en la consola del navegador.
+
+let miEdad = 22;
+let tuEdad = 30;
+
+console.log(`Tengo ${miEdad}\ntu tienes ${tuEdad} años`);
---
+++ Dia2/dia2_nivel1.js
@@ -0,0 +1,5 @@
+//1. Declare una variable llamada desafío y asígnele un valor inicial '30 días de JavaScript'.
+let desafio = "30 dias de JavaScript";
+
+//2. Imprima la cadena en la consola del navegador usando console.log()
+console.log(desafio);
